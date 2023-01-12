e31c41848fa8ecd87264f9d7d1b10db3ce4a8f58
Cogin <cevinmontgomery05@gmail.com>
2023-01-12 11:39:11 -0600

    Three Equal

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,15 @@
 public class ThreeEqual {
     public static void main(String[] args) {
-        
+        if (args.length != 3) {
+            System.out.println("3 Arguments expected, got " + args.length);
+            System.exit(1);
+        }
+
+        Integer x1 = Integer.parseInt(args[0]);
+        Integer x2 = Integer.parseInt(args[1]);
+        Integer x3 = Integer.parseInt(args[2]);
+
+        String equalmsg = (x1 == x2 && x1 == x3) ? "equal" : "not equal";
+        System.out.println(equalmsg);
     }
 }
